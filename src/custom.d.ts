4439b154eb7a92b56c362e9e21544022a1f77313
Valentino Conti <valeen.conti@hotmail.com>
2024-05-04 02:27:31 -0300

    video landing page

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,2 +1,10 @@
 declare module "*.svg";
 declare module "*.png";
+declare module "*.webm" {
+  const src: string;
+  export default src;
+}
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
 import favicon from "../../favicon.png";
+import porticoVertical from "../../assets/porticoVertical.webm";
+import porticoVerticalMp4 from "../../assets/porticoVertical.mp4";
+import porticoHorizontal from "../../assets/porticoHorizontal.webm";
+import porticoHorizontalMp4 from "../../assets/porticoHorizontal.mp4";
 import "./styles.scss";
+import { useWindowSize } from "src/utils/useWindowSize";
 
 const HomePage: React.FC = () => {
-  const [pressed, setPressed] = useState(false);
-
-  const handlePress = () => {
-    setPressed(true);
-    setTimeout(() => setPressed(false), 1800);
-  };
+  const { height, width } = useWindowSize();
+  const isHorizontal = width > height;
 
   return (
     <div className="home">
-      <h1>Bienvenido a Portico!</h1>
-      <img src={favicon} alt="Logo de Portico" />
-      <p>Explora nuestra colección exclusiva de productos</p>
-      <button onClick={handlePress} className="shop-button">
-        Ir a la Tienda
-      </button>
-
-      {pressed && (
-        <div style={{ marginTop: 12 }}>Psst.. Aún no! Próximamente</div>
-      )}
+      <div className="video-background">
+        <div className="video-background-container">
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline={true}
+            key={isHorizontal ? "HH" : "VV"}
+          >
+            <source
+              src={isHorizontal ? porticoHorizontal : porticoVertical}
+              type="video/webm"
+            />
+            <source
+              src={isHorizontal ? porticoHorizontalMp4 : porticoVerticalMp4}
+              type="video/mp4"
+            />
+            NOT SUPPORT
+          </video>
+        </div>
+      </div>
     </div>
   );
 };
