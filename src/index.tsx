2000e8cf6ead8bad245d0a53c2b50520b32963af
Valentino Conti <valeen.conti@hotmail.com>
2024-11-05 15:08:05 -0300

    fix URL instagram and new product

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 
-const root = createRoot(document.getElementById("app")!);
-root.render(<App />);
+// Add URL cleanup before rendering
+if (window.location.href.includes("%23")) {
+  const cleanUrl = window.location.href
+    .replaceAll("%23", "#")
+    .replaceAll("%2F", "/");
+
+  window.location.replace(cleanUrl);
+} else {
+  const root = createRoot(document.getElementById("app")!);
+  root.render(<App />);
+}
